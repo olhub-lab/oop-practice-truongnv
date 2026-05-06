b6038223783a9905136b4c9e426453ef42766ca0
FireFish <truongboboy2005@gmail.com>
2026-05-06 20:22:45 +0700

    feat: implement restful api delete order

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,6 +72,12 @@ public class OrderController {
       @RequestBody UpdateOrderRequest updateOrderRequest) {
     OrderResponse orderResponse = orderFacade.updateOrder(orderId, updateOrderRequest);
     return ResponseEntity.ok(orderResponse);
+  }
+
+  @DeleteMapping("/{orderId}")
+  public ResponseEntity<OrderResponse> deleteOrder(@PathVariable String orderId) {
+    orderFacade.deleteOrder(orderId);
+    return ResponseEntity.noContent().build();
   }
 
 }
